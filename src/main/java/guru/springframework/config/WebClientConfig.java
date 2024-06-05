a55c322359831c29cb0411859a2d9ed485e06d4b
ivanxalie <ivanxalie@gmail.com>
2024-06-05 20:37:02 +0300

    Add logbook

--- src/main/java/guru/springframework/config/WebClientConfig.java
+++ src/main/java/guru/springframework/config/WebClientConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.spring.webflux.LogbookExchangeFilterFunction;
 
 @Configuration
 public class WebClientConfig implements WebClientCustomizer {
@@ -28,6 +30,12 @@ public class WebClientConfig implements WebClientCustomizer {
                 new ServerOAuth2AuthorizedClientExchangeFilterFunction(manager);
         function.setDefaultClientRegistrationId(clientName);
 
-        webClientBuilder.filter(function).baseUrl(rootUrl);
+        LogbookExchangeFilterFunction logbookExchangeFilterFunction =
+                new LogbookExchangeFilterFunction(Logbook.create());
+
+        webClientBuilder
+                .filter(function)
+                .filter(logbookExchangeFilterFunction)
+                .baseUrl(rootUrl);
     }
 }
